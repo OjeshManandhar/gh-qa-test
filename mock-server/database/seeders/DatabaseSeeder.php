98f8c48f8b9ae7beb3f6bab6c88f47bef7ba503f
OjeshManandhar <ozes.manandhar@gmail.com>
2023-02-18 12:21:59 +0545

    Create user roles

--- mock-server/database/seeders/DatabaseSeeder.php
+++ mock-server/database/seeders/DatabaseSeeder.php
@@ -4,19 +4,25 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void {
+    // create roles
+    DB::table('roles')->insert(['name' => 'admin']);
+    DB::table('roles')->insert(['name' => 'user']);
+    DB::table('roles')->insert(['name' => 'guest']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    // create admin
+    \App\Models\User::factory()->create([
+      'name' => 'admin',
+      'email' => 'admin@test.com',
+      'role' => 'admin',
+    ]);
+
+    // \App\Models\User::factory(10)->create();
+  }
 }
