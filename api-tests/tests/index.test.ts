7e8c2f27200a1a63a7ef8fd6c49f68f64d322a84
OjeshManandhar <ozes.manandhar@gmail.com>
2023-02-18 23:33:09 +0545

    Setup packages

---
+++ api-tests/tests/index.test.ts
@@ -0,0 +1,11 @@
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+test('adds 3 + 2 to equal 3', () => {
+  expect(3 + 2).toBe(4);
+});
+
+test('adds 3 + 2 to equal 3', () => {
+  expect(3 + 2).toBe(5);
+});
