4858dd9fa05b770fa5ec7e927e34c6a3c1f87e27
OjeshManandhar <ozes.manandhar@gmail.com>
2023-02-19 13:39:55 +0545

    Test for creating an item

--- api-tests/tests/index.test.ts
+++ api-tests/tests/index.test.ts
@@ -25,6 +25,102 @@ describe('Item api', () => {
     userToken = userLogin.data.token;
   });
 
+  describe('Create item', () => {
+    const itemToCreate = {
+      name: 'test item',
+      description: 'test description',
+      price: 100,
+    };
+
+    it('should throw 401 error when not logged in', done => {
+      request(API_URL)
+        .post('/items')
+        .set('Accept', 'application/json')
+        .send(itemToCreate)
+        .end((err, res) => {
+          const body = res.body;
+
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(401);
+          expect(body).to.have.property('message');
+          done();
+        });
+    });
+
+    it('should create item when logged in as user', done => {
+      request(API_URL)
+        .post('/items')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${userToken}`)
+        .send(itemToCreate)
+        .end((err, res) => {
+          const body = res.body;
+
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(201);
+          expect(body).to.have.property('id');
+          expect(body).to.have.property('userId');
+          expect(body).to.have.property('name').that.equal(itemToCreate.name);
+          expect(body)
+            .to.have.property('description')
+            .that.equal(itemToCreate.description);
+          expect(body).to.have.property('price').that.equal(itemToCreate.price);
+          done();
+        });
+    });
+
+    it('should create item when logged in as admin', done => {
+      request(API_URL)
+        .post('/items')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send(itemToCreate)
+        .end((err, res) => {
+          const body = res.body;
+
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(201);
+          expect(body).to.have.property('id');
+          expect(body).to.have.property('userId');
+          expect(body).to.have.property('name').that.equal(itemToCreate.name);
+          expect(body)
+            .to.have.property('description')
+            .that.equal(itemToCreate.description);
+          expect(body).to.have.property('price').that.equal(itemToCreate.price);
+          done();
+        });
+    });
+
+    it('should throw 422 error when invalid input is provided', done => {
+      request(API_URL)
+        .post('/items')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send({
+          name: 'n',
+          description: 'd',
+          price: -101,
+        })
+        .end((err, res) => {
+          const body = res.body;
+
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(422);
+          expect(body).to.have.property('message');
+          expect(body).to.have.property('errors');
+          // Because provided name was invalid
+          expect(body.errors).to.have.property('name').that.is.an('array');
+          // Because provided description was invalid
+          expect(body.errors)
+            .to.have.property('description')
+            .that.is.an('array');
+          // Because provided price was invalid
+          expect(body.errors).to.have.property('price').that.is.an('array');
+          done();
+        });
+    });
+  });
+
   describe('Fetch items', () => {
     let firstItem: {
       id: string;
