6e56df4098be7a871af3f8670e715071db05a62d
OjeshManandhar <ozes.manandhar@gmail.com>
2023-02-19 13:28:20 +0545

    Test for fetching all items

--- api-tests/tests/index.test.ts
+++ api-tests/tests/index.test.ts
@@ -9,14 +9,14 @@ import request from 'supertest';
 const API_URL = process.env.API_URL;
 
 describe('Item api', () => {
-  let userToken: string, authToken: string;
+  let userToken: string, adminToken: string;
 
   beforeAll(async () => {
     const adminLogin = await axios.post(`${API_URL}/auth/login`, {
       email: 'admin@test.com',
       password: 'password',
     });
-    authToken = adminLogin.data.token;
+    adminToken = adminLogin.data.token;
 
     const userLogin = await axios.post(`${API_URL}/auth/login`, {
       email: 'test@test.com',
@@ -26,19 +26,72 @@ describe('Item api', () => {
   });
 
   describe('Fetch items', () => {
+    let firstItem: {
+      id: string;
+      userId: string;
+      name: string;
+      description: string;
+      price: number;
+    };
+
     it('should return items array when not authenticated', done => {
       request(API_URL)
         .get('/items')
         .set('Accept', 'application/json')
         .end((err, res) => {
           const body = res.body;
-          console.log('res:', body);
+
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(body).to.be.an('array').that.include.members;
+
+          firstItem = body[0];
+          expect(firstItem.id).to.be.a('string');
+          expect(firstItem.userId).to.be.a('string');
+          expect(firstItem.name).to.be.a('string');
+          expect(firstItem.description).to.be.a('string');
+          expect(firstItem.price).to.be.a('number');
+
+          done();
+        });
+    });
+
+    it('should return items array when logged in as user', done => {
+      request(API_URL)
+        .get('/items')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${userToken}`)
+        .end((err, res) => {
+          const body = res.body;
 
           expect(err).to.be.null;
           expect(res.statusCode).to.equal(200);
           expect(body).to.be.an('array');
           done();
         });
+    });
+
+    it('should return items array when logged in as admin', done => {
+      request(API_URL)
+        .get('/items')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .end((err, res) => {
+          const body = res.body;
+
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(body).to.be.an('array');
+          done();
+        });
+    });
+
+    it('should return item array with this structure', () => {
+      expect(firstItem.id).to.be.a('string');
+      expect(firstItem.userId).to.be.a('string');
+      expect(firstItem.name).to.be.a('string');
+      expect(firstItem.description).to.be.a('string');
+      expect(firstItem.price).to.be.a('number');
     });
   });
 });
